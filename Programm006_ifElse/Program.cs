bcd7ed8ac5557332f2e3c72d305e124548e15a82
Dara Kosova <darakosova.gb@gmail.com>
2022-06-23 23:53:30 +0400

    Commit for Programm 6

---
+++ Programm006_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "даша")
+{
+    Console.WriteLine("Gucci flip flop, DaShA!");
+} 
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
